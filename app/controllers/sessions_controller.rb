37d3a394ba02dcb1a0c47523f7f888946b703dc6
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-10-27 02:49:00 -0600

    implementing registration controller and final authentication methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
-class SessionStore < ApplicationController
+class SessionsController < ApplicationController
+  include CurrentUserConcern
   def create
     user = User.find_by(email: params[:user][:email])
                .try(:authenticate, params[:user][:password])
@@ -13,5 +14,23 @@ class SessionStore < ApplicationController
     else
       render json: { status: 401 }
     end
+  end
+
+  def logged_in
+    if @current_user
+      render json: {
+        logged_in: true,
+        user: @current_user
+      }
+    else
+      render json: {
+        logged_in: false
+      }
+    end
+  end
+
+  def logout
+    reset_session
+    render json: { status: 200, logged_out: true }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
   root to: "static#home"
 end
