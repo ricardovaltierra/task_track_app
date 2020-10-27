1d31d4dc612dc3b6c1aefab87d0d28f27919dc72
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-10-26 18:19:50 -0600

    fixed cors and session_store

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     end
 
   allow do
-    origins "http://tracking-app-api.herokuapp.com"
+    origins "https://steptracking-api.herokuapp.com/"
     resource "*", headers: :any, methods: [:get, :post, :put, :delete, :patch, :options, :head], credentials: true
   end
     
