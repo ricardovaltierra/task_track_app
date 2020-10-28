8ed4f51c54881a522aeeefefec92de2a0a4a2c82
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-10-28 09:43:56 -0600

    session test on change domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_track_auth", domain: "steptracking-api.herokuapp.com/"
+  Rails.application.config.session_store :cookie_store, key: "_track_auth", domain: "https://steptracking-api.herokuapp.com/"
 else
   Rails.application.config.session_store :cookie_store, key: "_track_auth"
 end
