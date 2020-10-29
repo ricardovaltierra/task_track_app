2868845e7a8c11a4d6d04ee071208469478401e8
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-10-29 03:44:40 -0600

    set up conditional on session_store and symbol quotes exchage

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,9 @@
 class RegistrationsController < ApplicationController
   def create
     user = User.create!(
-      email: params[:user][:email],
-      password: params[:user][:password],
-      password_confirmation: params[:user][:password_confirmation]
+      email: params['user']['email'],
+      password: params['user']['password'],
+      password_confirmation: params['user']['password_confirmation']
     )
 
     if user
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
   include CurrentUserConcern
   def create
-    user = User.find_by(email: params[:user][:email])
-               .try(:authenticate, params[:user][:password])
+    user = User.find_by(email: params['user']['email'])
+               .try(:authenticate, params['user']['password'])
 
     if user
       session[:user_id] = user.id
