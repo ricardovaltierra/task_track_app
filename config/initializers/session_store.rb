3b9584db7f88b5a5033a878e7ccc4c725dcc01ec
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-10-28 23:09:50 -0600

    domain test for session_store in production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: "_track_auth", domain: "steptracking-api.herokuapp.com/"
+Rails.application.config.session_store :cookie_store, key: "_track_auth", :domain => :all
