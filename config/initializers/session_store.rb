a801ca7c7ac9679433a02a7a1dafb5d492474b4c
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-10-30 00:28:45 -0600

    session_store update with smae_site constraint

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app", :domain => :all
+  Rails.application.config.session_store :cookie_store, key: "_track_app", same_site: none, secure: true
 else
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+  Rails.application.config.session_store :cookie_store, key: "_track_app"
 end
