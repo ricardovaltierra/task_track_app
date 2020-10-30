702ff61c8c7cb290f4fd5b5052077d98c8fd3ea6
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-10-30 00:35:29 -0600

    fix none symbol on session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_track_app", same_site: none, secure: true
+  Rails.application.config.session_store :cookie_store, key: "_track_app", same_site: :none, secure: true
 else
   Rails.application.config.session_store :cookie_store, key: "_track_app"
 end
