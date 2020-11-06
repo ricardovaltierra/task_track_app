f49c9d368cde0f787aae233ef49c447109402efb
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-11-06 15:27:59 -0600

    updated CORS with localhost origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,4 +4,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       origins "https://steptracking.herokuapp.com/"
       resource "*", headers: :any, methods: [:get, :post, :put, :delete, :patch, :options, :head], credentials: true
     end
+
+    allow do
+      origins "http://localhost:3000"
+      resource "*", headers: :any, methods: [:get, :post, :put, :delete, :patch, :options, :head], credentials: true
+    end
 end
