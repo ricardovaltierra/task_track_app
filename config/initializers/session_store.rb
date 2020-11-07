f2fba6d243818846ef22512a180fb96e7185bc02
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-11-07 12:07:52 -0600

    CORS for localhost removed and updated session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_track_app", same_site: :none
+  Rails.application.config.session_store :cookie_store, key: "_track_app", same_site: :Strict, secure: true
 else
   Rails.application.config.session_store :cookie_store, key: "_track_app"
 end
