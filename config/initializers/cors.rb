6dcb4cd513b2524a487c4e0d09a630ade10fd31e
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-11-07 10:38:29 -0600

    cors udate with updated values

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ["http://localhost:3000", "https://steptracking-api.herokuapp.com"]
+    origins ["https://steptracking.herokuapp.com/", "http://localhost:3000"]
 
     resource "*", 
     headers: :any, methods: [:get, :post, :put, :delete, :patch, :options, :head], 
     credentials: true
   end
    
+  
 end
