1a7ad3eb905c2cff0622f42581ae42edcf1a8e65
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-11-07 13:37:45 -0600

    updated cors with neetlify domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ["https://steptracking.herokuapp.com/", "http://localhost:3000"]
+    origins ["https://steptracking.netlify.app/", "http://localhost:3000"]
     resource "*", 
     headers: :any, methods: [:get, :post, :put, :delete, :patch, :options, :head], 
     credentials: true
