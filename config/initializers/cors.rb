9342d06dc45cff69e93fae4b1557d60470ed2be4
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-11-07 11:30:00 -0600

    CORS for localhost removed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ["https://steptracking.herokuapp.com/", "http://localhost:3000"]
+    origins ["https://steptracking.herokuapp.com/"]
 
     resource "*", 
     headers: :any, methods: [:get, :post, :put, :delete, :patch, :options, :head], 
