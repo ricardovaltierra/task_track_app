72ce4323b68181ca5457efd5d6c4b42d30b4f34b
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-11-08 03:45:29 -0600

    model update to delete tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   belongs_to :user
   has_many :records, dependent: :destroy
   validates :name, presence: true, uniqueness: true
-  validates :description, presence: true, uniqueness: true
+  validates :description, presence: true
   validates :completion, presence: true
   validates :user_id, presence: true
 end
