de89051e89f41b7e4963b156e651f358705a6745
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-11-08 05:11:30 -0600

    route to reset task updated

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:create]
   resources :registrations, only: [:create, :destroy]
 
-  delete :destroy, to: "tasks#destroy"
+  delete :reset, to: "tasks#destroy"
 
   resources :tasks, only: [:index, :show, :create, :update]
 
