657af431df1b4e3cded7191fce12d1eadcd92078
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-11-08 03:54:03 -0600

    model update to delete tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :tasks, dependent: :destroy
-  has_many :records
+  has_many :records, dependent: :destroy
   validates_presence_of :email
   validates_uniqueness_of :email
 end
