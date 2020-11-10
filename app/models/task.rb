4f319d500945ccc9c556e715f31e80a91f6692aa
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-11-10 15:11:38 -0600

    update task uniqueness scope

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
   has_many :records, dependent: :destroy
-  validates :name, presence: true, uniqueness: { scope: :user_id }
+  validates :name, presence: true, uniqueness: true
   validates :description, presence: true
   validates :completion, presence: true
   validates :user_id, presence: true
